df2e6f3963c061db44ee1d1da8757b515e2d68e1
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-06-05 16:04:01 +0000

    chore(models): adding user model example

---
+++ example/api/user/user.model.ts
@@ -0,0 +1,24 @@
+import { Entity, Field } from 'sls-api-decorators/lib/models';
+import * as Debug from 'debug'
+const $log = console.log;
+
+@Entity({
+  table: 'Users',
+  storage: 'DynamoDB'
+})
+export class User {
+
+  @Field()
+  public id: string;
+
+  @Field()
+  public name: string;
+
+  @Field()
+  public email: string;
+
+
+  constructor() {
+
+  }
+}
