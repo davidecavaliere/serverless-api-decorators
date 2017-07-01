4d780366011678e641cbfa2857e5793129102dd4
Davide Cavaliere <cavaliere.davide@gmail.com>
2017-07-01 15:41:44 +0000

    chore(example): aligning example

--- example/api/index.ts
+++ example/api/index.ts
@@ -3,12 +3,5 @@ import { App } from './app';
 
 const app = new App();
 
-console.log(app);
-console.log('services', app.services);
-
-// export const _app = app.service.userService;
-
-
-
-export const echo  = app.services.userService.list;
-export const hello = app.services.userService.error;
+console.log('exporting app', app);
+export = app;
--- example/handler.ts
+++ example/handler.ts
@@ -1,11 +1,12 @@
 
 import { App } from './api/app';
 const app = new App();
-
-console.log(app);
-console.log('services', app.services);
-
-export const error = app.services.userService.error;
-
-export const hello = app.services.userService.list;
     
+export const list = app.services.userService.list;
+          
+export const getById = app.services.userService.getById;
+          
+export const getSubscriptions = app.services.userService.getSubscriptions;
+          
+export const error = app.services.userService.error;
+          
